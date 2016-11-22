697948ff2cffa7f9e52ef9ad07e7e4f02ecc1b62
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:11:50 +0000

    Generate lists resource with starter:resource

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,81 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+
+    render("lists/index.html.erb")
+  end
+
+  def show
+    @list = List.find(params[:id])
+
+    render("lists/show.html.erb")
+  end
+
+  def new
+    @list = List.new
+
+    render("lists/new.html.erb")
+  end
+
+  def create
+    @list = List.new
+
+    @list.title = params[:title]
+    @list.board_id = params[:board_id]
+
+    save_status = @list.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/lists/new", "/create_list"
+        redirect_to("/lists")
+      else
+        redirect_back(:fallback_location => "/", :notice => "List created successfully.")
+      end
+    else
+      render("lists/new.html.erb")
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+
+    render("lists/edit.html.erb")
+  end
+
+  def update
+    @list = List.find(params[:id])
+
+    @list.title = params[:title]
+    @list.board_id = params[:board_id]
+
+    save_status = @list.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/lists/#{@list.id}/edit", "/update_list"
+        redirect_to("/lists/#{@list.id}", :notice => "List updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "List updated successfully.")
+      end
+    else
+      render("lists/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+
+    @list.destroy
+
+    if URI(request.referer).path == "/lists/#{@list.id}"
+      redirect_to("/", :notice => "List deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "List deleted.")
+    end
+  end
+end
---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+end
