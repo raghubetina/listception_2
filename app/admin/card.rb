908a87ab8e2ce0033f6b4aea8d690433f194a0d7
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:12:30 +0000

    Whitelist columns in Card for ActiveAdmin

--- app/admin/card.rb
+++ app/admin/card.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Card do
 
+ permit_params :title, :description, :list_id, :image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
