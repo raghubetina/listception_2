5b95921c7edfb5f608a194279b8381b45559815d
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:12:27 +0000

    Whitelist columns in List for ActiveAdmin

--- app/admin/list.rb
+++ app/admin/list.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register List do
 
+ permit_params :title, :board_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
