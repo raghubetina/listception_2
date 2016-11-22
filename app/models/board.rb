91b114e6cb5d65969ecd0570c5ebc2867a5e7d4e
Raghu Betina <raghu@firstdraft.com>
2016-11-22 05:12:02 +0000

    Validate uniqueness of boards.title

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,6 +5,8 @@ class Board < ApplicationRecord
 
   # Validations
 
+  validates :title, :uniqueness => { :scope => [:user_id] }
+
   validates :title, :presence => true
 
   validates :user_id, :presence => true
